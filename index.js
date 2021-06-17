ebf8dcc73c37653a9e65ece8c488a361ff670e31
madelacerda <53839267+madelacerda@users.noreply.github.com>
2021-06-17 18:27:21 -0400

    server

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-const express = require("express");
-const conectarDB = require("./config/db");
-const cors = require("cors");
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
 
-//Crear el servidor
+// crear el servidor
 const app = express();
 
-// Conectar a la bade de datos
+// Conectar a la base de datos
 conectarDB();
 
-//Habilitar cors
+// habilitar cors
 app.use(cors());
 
-// Habilñitar express.json
-app.use(express.json({extended: true}));
+// Habilitar express.json
+app.use( express.json({ extended: true }));
 
-// Puerto de la app
-const port = process.env.port || 4000;
+// puerto de la app
+const port = process.env.PORT || 4000;
 
-//Importar rutas
+// Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
-// Definir la pagina principal
-app.get("/", (req, res) => {
-  res.send("Hola Abr");
-});
 
-//Arrancar la app
-app.listen(port, '0.0.0.0' ,  () => {
-  console.log(`El servidor esta funcionando en el puerto ${port}`);
+// arrancar la app
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 });
